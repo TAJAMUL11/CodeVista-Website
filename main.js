25ae9e9d448d21e5f66e7de5ddfdbd1ceb6a9e2e
Tajamul <thetj4054@gmail.com>
2024-06-26 19:46:48 +0530

    Updated UI and added new images and design

--- main.js
+++ main.js
@@ -1,54 +1,20 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
+const menuBtnIcon = document.querySelector("i");
+const header = document.getElementById("header");
+console.log(header);
+window.addEventListener('scroll',() => {
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
 
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
-
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
-
-navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
-});
-
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
-};
-
-// header container
-ScrollReveal().reveal("home__container h1", {
-  ...scrollRevealOption,
-});
-
-ScrollReveal().reveal("home__container p", {
-  ...scrollRevealOption,
-  delay: 500,
-});
-
-ScrollReveal().reveal("home__container form", {
-  ...scrollRevealOption,
-  delay: 1000,
-});
-
-ScrollReveal().reveal("home__container a", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-ScrollReveal().reveal("home__container #applyNow", {
-  ...scrollRevealOption,
-  delay: 5500,
-});
-
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper',{
+  effect: 'slide',
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
+  autoplay : {
+    delay : 1500,
   },
-});
+})
+
+
 
 
